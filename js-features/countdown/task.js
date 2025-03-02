bbd127236abd31c1350d2c697a1c06d34736ab85
Denis Garpinich <d.garpinich@wisetech.pro>
2025-03-02 18:51:55 +0300

    Задание 1.1. полностью выполнено

--- js-features/countdown/task.js
+++ js-features/countdown/task.js
@@ -1,11 +1,13 @@
 const timer = document.getElementById('timer');
-// timer.textContent = '01:00:05';
+// timer.textContent = '00:00:05';
 const id = setInterval(() => {
   timer.textContent =
       convertToHhMmSs(parseInt(timeToSeconds(timer.textContent)) - 1);
   if (timer.textContent === '00:00:00') {
     clearInterval(id);
     alert('Вы победили в конкурсе!');
+    window.location =
+        'https://github.com/GarDeN-bear/bhj-homeworks/tree/master/js-features/countdown';
   }
 }, 1000);
 
@@ -14,7 +16,7 @@ function convertToHhMmSs(seconds) {
   const mm =
       Math.floor(Math.abs((seconds % 3600) / 60)).toString().padStart(2, '0');
   const ss = (seconds % 60).toString().padStart(2, '0');
-  console.log(seconds);
+  //   console.log(seconds);
   return `${hh}:${mm}:${ss}`;
 }
 
