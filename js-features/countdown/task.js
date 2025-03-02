0b520d26b6fc5f4f1dedaa4a0715342abbe09e5c
Denis Garpinich <d.garpinich@wisetech.pro>
2025-03-02 18:37:33 +0300

    Задание 1.1 Повышенная сложность 1 выполнена

--- js-features/countdown/task.js
+++ js-features/countdown/task.js
@@ -1,10 +1,24 @@
 const timer = document.getElementById('timer');
-timer.textContent = 5;
+// timer.textContent = '01:00:05';
 const id = setInterval(() => {
-  timer.textContent = parseInt(timer.textContent) - 1;
-  if (timer.textContent === '0') {
+  timer.textContent =
+      convertToHhMmSs(parseInt(timeToSeconds(timer.textContent)) - 1);
+  if (timer.textContent === '00:00:00') {
     clearInterval(id);
     alert('Вы победили в конкурсе!');
   }
 }, 1000);
-console.log(timer);
+
+function convertToHhMmSs(seconds) {
+  const hh = Math.floor(Math.abs(seconds / 3600)).toString().padStart(2, '0');
+  const mm =
+      Math.floor(Math.abs((seconds % 3600) / 60)).toString().padStart(2, '0');
+  const ss = (seconds % 60).toString().padStart(2, '0');
+  console.log(seconds);
+  return `${hh}:${mm}:${ss}`;
+}
+
+function timeToSeconds(seconds) {
+  const [hh, mm, ss] = seconds.split(':').map(Number);
+  return parseInt(hh * 3600 + mm * 60 + ss);
+}
