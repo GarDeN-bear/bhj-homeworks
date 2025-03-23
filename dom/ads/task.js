a9b4c4efde06f9ae5ce6ed8313ded2375b728b8a
Denis Garpinich <d.garpinich@wisetech.pro>
2025-03-23 16:44:10 +0300

    DOM Задание 2.1 Полностью выполнено

--- dom/ads/task.js
+++ dom/ads/task.js
@@ -1,5 +1,6 @@
 let rotatorCases = Array.from(document.getElementsByClassName('rotator__case'));
 let indexActiveCase = 0;
+let id;
 
 function setCase() {
   rotatorCases[indexActiveCase].classList.remove('rotator__case_active');
@@ -8,6 +9,21 @@ function setCase() {
     indexActiveCase = 0;
   }
   rotatorCases[indexActiveCase].classList.add('rotator__case_active');
+  rotatorCases[indexActiveCase].style.color =
+      rotatorCases[indexActiveCase].getAttribute('data-color');
+  clearInterval(id);
+  id = setInterval(
+      setCase, rotatorCases[indexActiveCase].getAttribute('data-speed'));
 }
 
-setInterval(setCase, 1000);
+let colors = Array('red', 'green', 'blue');
+let speeds = Array(1000, 500, 2000);
+
+rotatorCases.forEach((el) => {
+  el.setAttribute(
+      'data-color', colors[Math.floor(Math.random() * colors.length)]);
+  el.setAttribute(
+      'data-speed', speeds[Math.floor(Math.random() * speeds.length)])
+});
+
+setCase();
