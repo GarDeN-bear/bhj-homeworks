f3a5e02b8e69c15ebe338bb3b1223e75774140a6
Denis Garpinich <d.garpinich@wisetech.pro>
2025-03-29 21:11:35 +0300

    Изменение структуры HTML-документа. Правки задания 2

--- document-structure/todo/task.js
+++ document-structure/todo/task.js
@@ -1,37 +1,29 @@
 let taskAdd = document.getElementById('tasks__add');
 let taskInput = document.getElementById('task__input');
-let taskList = document.getElementById('tasks__list');
+let tasksList = document.getElementById('tasks__list');
 
-function createHtmlElement(text) {
-  let div1 = document.createElement('div');
-  let div2 = document.createElement('div');
-  let a = document.createElement('a');
-
-  div1.classList.add('task');
-  div2.classList.add('task__title');
-  a.setAttribute('href', '#');
-  a.classList.add('task__remove');
-  a.textContent = 'X';
-  a.addEventListener('click', (event) => {
+function createHtmlElement(title) {
+  tasksList.insertAdjacentHTML('afterbegin', `
+        <div class="task">
+          <div class="task__title">
+            ${title}
+          </div>
+          <a href="#" class="task__remove">X</a>
+        </div>
+    `);
+  let lastTaskRemove =
+      Array.from(tasksList.getElementsByClassName('task__remove'))[0];
+  let lastTask = Array.from(tasksList.getElementsByClassName('task'))[0];
+  lastTaskRemove.addEventListener('click', (event) => {
     event.preventDefault();
-    div1.remove();
+    lastTask.remove();
   });
-  div2.textContent = text;
-  div1.append(div2, a);
-  taskList.append(div1);
 }
 
 taskAdd.addEventListener('click', (event) => {
   event.preventDefault();
 
   if (!taskInput.value.length == 0) {
-    createHtmlElement(taskInput.value);
-  }
-});
-
-document.addEventListener('keydown', function(event) {
-  event.preventDefault();
-  if ((event.key === 'Enter') && !taskInput.value.length == 0) {
     createHtmlElement(taskInput.value);
   }
 });
