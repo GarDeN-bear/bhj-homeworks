27a30f7dc512715d39882ecde086e4ffbe9d6ccd
Denis Garpinich <d.garpinich@wisetech.pro>
2025-04-02 09:04:21 +0300

    Хранение состояния на клиенте. Задание 3 выполнено

---
+++ client-state/auth/task.js
@@ -0,0 +1,38 @@
+let form = document.getElementById('signin__form');
+
+if (localStorage.getItem('userId')) {
+  document.getElementById('signin').classList.remove('signin_active');
+  document.getElementById('welcome').classList.add('welcome_active');
+  document.getElementById('user_id').textContent =
+      localStorage.getItem('userId');
+} else {
+  document.getElementById('signin').classList.add('signin_active');
+}
+
+form.addEventListener('submit', (event) => {
+  event.preventDefault();
+  let formData = new FormData(form);
+
+  let xhr = new XMLHttpRequest();
+  xhr.open('POST', 'https://students.netoservices.ru/nestjs-backend/auth');
+
+  xhr.addEventListener('readystatechange', () => {
+    if (xhr.readyState === xhr.DONE) {
+      handleResponse(JSON.parse(xhr.response));
+    }
+  });
+
+
+  xhr.send(formData);
+});
+
+function handleResponse(response) {
+  if (response.success) {
+    document.getElementById('welcome').classList.add('welcome_active');
+    document.getElementById('user_id').textContent = response.user_id;
+    localStorage.setItem('userId', response.user_id);
+    document.getElementById('signin').classList.remove('signin_active');
+  } else {
+    alert('Неверный логин/пароль');
+  }
+}
