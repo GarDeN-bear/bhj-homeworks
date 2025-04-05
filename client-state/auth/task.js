48f9c7868fcfc240ce874efad7a6b693763152db
Denis Garpinich <d.garpinich@wisetech.pro>
2025-04-05 22:11:45 +0300

    Хранение состояния на клиенте. Задание 3 исправлено

--- client-state/auth/task.js
+++ client-state/auth/task.js
@@ -1,11 +1,9 @@
 let form = document.getElementById('signin__form');
+
 if (localStorage.getItem('userId')) {
-  document.getElementById('signin').classList.remove('signin_active');
-  document.getElementById('welcome').classList.add('welcome_active');
+  swapActiveElements();
   document.getElementById('user_id').textContent =
       localStorage.getItem('userId');
-} else {
-  document.getElementById('signin').classList.add('signin_active');
 }
 
 form.addEventListener('submit', (event) => {
@@ -15,29 +13,37 @@ form.addEventListener('submit', (event) => {
   let xhr = new XMLHttpRequest();
   xhr.open('POST', 'https://students.netoservices.ru/nestjs-backend/auth');
 
-  xhr.addEventListener('readystatechange', () => {
-    if (xhr.readyState === xhr.DONE) {
-      handleResponse(JSON.parse(xhr.response));
-    }
+  xhr.addEventListener('load', () => {
+    handleResponse(xhr.response);
   });
 
-
+  xhr.responseType = 'json';
   xhr.send(formData);
 });
 
+function swapActiveElements() {
+  if (document.getElementById('welcome').classList.contains('welcome_active')) {
+    document.getElementById('welcome').classList.remove('welcome_active');
+    document.getElementById('signin').classList.add('signin_active');
+  } else {
+    document.getElementById('welcome').classList.add('welcome_active');
+    document.getElementById('signin').classList.remove('signin_active');
+  }
+}
+
 function handleResponse(response) {
   if (response.success) {
-    document.getElementById('welcome').classList.add('welcome_active');
     document.getElementById('user_id').textContent = response.user_id;
     localStorage.setItem('userId', response.user_id);
-    document.getElementById('signin').classList.remove('signin_active');
+    swapActiveElements();
   } else {
     alert('Неверный логин/пароль');
     location.reload();
   }
+  form.reset();
 }
 
 document.getElementById('signout__btn').addEventListener('click', () => {
   localStorage.removeItem('userId');
-  location.reload();
+  swapActiveElements();
 });
