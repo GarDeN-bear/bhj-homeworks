d06587c416512b9d0806aa32b996ef6252a42aaf
Denis Garpinich <d.garpinich@wisetech.pro>
2025-04-05 21:43:43 +0300

    Хранение состояния на клиенте. Задание 2 исправлено

--- client-state/popup/task.js
+++ client-state/popup/task.js
@@ -1,25 +1,12 @@
 
-let body = document.body;
-
-body.insertAdjacentHTML(
-    'afterbegin', `<div class="modal modal_active" id="subscribe-modal">
-    <div class="modal__content">
-        <div class="modal__close modal__close_times">&times;</div>
-        Подпишитесь на нашу рассылку пожалуйста!
-    </div>
-</div>`);
-
 let cookiePairs = document.cookie.split('; ');
 let popupState = cookiePairs.find(p => p.startsWith('popupState='));
-let modal = body.querySelector('.modal');
-if (popupState) {
-  modal.classList.remove('modal_active');
-} else {
+let modal = document.getElementById('subscribe-modal');
+if (!popupState) {
   modal.classList.add('modal_active');
 }
 
-
-let popupButton = body.querySelector('.modal__close');
+let popupButton = document.body.querySelector('.modal__close');
 popupButton.addEventListener('click', () => {
   if (modal.classList.contains('modal_active')) {
     modal.classList.remove('modal_active');
